93d90339258318a1c3acfe75b721ba3f8a70f237
Alexander Popov <alex.val.popov@gmail.com>
2017-07-31 12:18:38 +0300

    Exercise 1.8

---
+++ chapter1/8.scm
@@ -0,0 +1,26 @@
+(define (abs x)
+  (if (< x 0)
+      (- x)
+      x))
+
+(define (square x)
+  (* x x))
+
+(define (cube x)
+  (* x x x))
+
+(define (improve guess x)
+  (/ (+ (/ x (square guess))
+        (* 2 guess))
+     3))
+
+(define (good-enough? guess x)
+  (< (abs (- (cube guess) x)) 0.001))
+
+(define (cubrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (cubrt-iter (improve guess x) x)))
+
+(define (cubrt x)
+  (cubrt-iter 1.0 x))
